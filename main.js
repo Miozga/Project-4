26d816ea5c0d93009291b265f5614c665e878a58
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-02-03 13:09:23 +0100

    3commit

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 document.body.addEventListener("mousemove", (e) => {
-  // console.log(e.clientX, e.clientY);
-  document.querySelector("h1").textContent = `${e.clientX}, ${e.clientY}`;
+  const x = e.clientX;
+  const y = e.clientY;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
+  // console.log(e.clientX, e.clientY);
+  document.querySelector("h1").textContent = `${x}, ${y}`;
+
+  const red = (x / width) * 100;
+  const green = (y / height) * 100;
+  const blue = (x / width) * 100 + ((y / height) * 100) / 2;
   //ustawienia nasłuchiwania zdarzenia ruchu myszką i przypisana funkcja do tego zdarzenia
-  document.body.style.backgroundColor = `rgb(${e.clientX / 3}, ${
-    e.clientY / 2
-  }, ${(e.clientX / e.clientY) * 10})`;
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
 });
