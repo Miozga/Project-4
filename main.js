1bf9494a72a85a4ff0afe3d5338f07556f268759
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-02-03 19:38:43 +0100

    end

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+/*
+Zmiana koloru po ruszaniu myszką
+
 document.body.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
+const x = e.clientX;
+const y = e.clientY;
   const width = window.innerWidth;
   const height = window.innerHeight;
 
@@ -12,4 +15,26 @@ document.body.addEventListener("mousemove", (e) => {
   const blue = (x / width) * 100 + ((y / height) * 100) / 2;
   //ustawienia nasłuchiwania zdarzenia ruchu myszką i przypisana funkcja do tego zdarzenia
   document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
+});
+*/
+
+document.body.addEventListener("click", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const getColor = (x, y) => {
+    if (x % 2 === 0 && y % 2 === 0) {
+      return "red";
+    } else if (x % 2 === 0) {
+      return "orange";
+    } else if (y % 2 === 0) {
+      return "blue";
+    } else {
+      return "silver";
+    }
+  };
+  const color = getColor(x, y);
+
+  console.log(x, y);
+  document.body.style.backgroundColor = color;
 });
